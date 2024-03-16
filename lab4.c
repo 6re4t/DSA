25a0f39cbc2edbbf34f666990a52670d45721ae7
Darius Carvalho <160844048+6re4t@users.noreply.github.com>
2024-03-16 17:35:13 +0400

    Fixed lab4.c
    can now be used with the data provided in GC.

--- lab4.c
+++ lab4.c
@@ -2,12 +2,29 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+ 
+void insertionSort(int elementsint[], int n) {
+    int i, temp, j;
+    for (i = 1; i <= n+1; i++) {
+        temp = elementsint[i];
+        j = i - 1;
 
-void linearSearch(int size, int elementsint[size], int num, FILE *outFile) {
+        while (j >= 0 && elementsint[j] > temp) {
+            elementsint[j + 1] = elementsint[j];
+            j = j - 1;
+        }
+        elementsint[j + 1] = temp;
+    }
+}
+
+void linearSearch(int size, int elementsint[], int num, FILE *outFile) {
     int flag = 0;
     clock_t start, end;                    //measure time take to search
     start = clock();
-    for (int i = 0; i < size; i++) {
+
+    insertionSort(elementsint, size);
+
+    for (int i = 0; i <= size+1; i++) {
         if (elementsint[i] == num) {
             end = clock();
             fprintf(outFile, "Input Position Search_time\n");
@@ -20,12 +37,15 @@ void linearSearch(int size, int elementsint[size], int num, FILE *outFile) {
     }
 }
 
-int binarySearch(int size, int elementsint[size], int num, FILE *outFile2) {
+int binarySearch(int size, int elementsint[], int num, FILE *outFile2) {
     int left = 0;
     int right = size-1;
     int mid = (left + right)/2;
     clock_t start, end;                  //meaure time taken to search
     start = clock();
+
+    insertionSort(elementsint, size);
+
     while (left <= right) {
         if (num > elementsint[mid]) {
             left = mid + 1;
@@ -47,24 +67,28 @@ int binarySearch(int size, int elementsint[size], int num, FILE *outFile2) {
 }
 
 int main() {
-    int size, num;
-    char a[2];
-    char elements[100];
-    
+    int size, num, line;
+    char a[20];
+    char *elements = (char *)malloc(3000000 * sizeof(char));
+
+    printf("Enter test case size from 1 to 15: ");
+    scanf("%d", &line);
+    if (line > 15 || line < 1) {
+        printf("Invalid size\n");
+        exit(1);
+    }
+
     FILE *fptr = fopen("testCaseSize.txt", "r");
     if (fptr == NULL) {
         printf("Error opening file!\n");
         exit(1);
     }
-    if (fgets(a, sizeof(a), fptr)) {
-        size = atoi(a);                                    //converting size from fgets string to int
-    }
-    fclose(fptr);
-    
-    if (size > 15) {
-        printf("Invalid size\n");
-        exit(1);
-    }
+
+    for(int i = 1; i <= line; i++) {
+        (fgets(a, sizeof(a), fptr));            //jumping to line of size provided by user in testCaseSize file
+        }
+        fclose(fptr);
+        size = atoi(a);
     
     printf("Enter number to be searched: ");
     scanf("%d", &num);
@@ -74,14 +98,17 @@ int main() {
         printf("Error opening file!\n");
         exit(1);
     }
-    fgets(elements, sizeof(elements), fptr2);
+
+    for(int i = 1; i <= line; i++) {
+        fgets(elements, 3000000, fptr2);        //saving all characters of that line in char array
+    }
     fclose(fptr2);
 
-    int elementsint[size];
+    int *elementsint = (int *)malloc(491520 * sizeof(int));
     int i = 0;
     char *token = strtok(elements, " ");
-    elementsint[0] = atoi(token);                      //using string tokenizer to separate individual numbers and store in array
-    while ((token = strtok(NULL, " ")) != NULL) {
+    elementsint[0] = atoi(token);
+    while ((token = strtok(NULL, " ")) != NULL) {            //tokenizing each individual element in the char array to an int array
         i++;
         elementsint[i] = atoi(token);
     }
